bfe9483cbea4482b862ef2c792dc8fe83b4c87b4
Wang, ChenYu git config --global  git config --global user.email <cwang929@insite.4cd.edu>
2022-10-03 15:49:40 -0700

    q1

--- q1.py
+++ q1.py
@@ -8,18 +8,22 @@
 # No need to use list
 # All input values are taken one by one separatively.
 ###
-even = 0
-number = 0
+even = int(0)
+number = int(0)
 while True:
     num = int(input())
-    if number > 10:
-        break
-    elif num % 2 == 0:
+    number += 1
+    
+    if num % 2 == 0:
         even += 1
-        number += 1
-        continue
+        if number == 10:
+            break
+        else: continue
     else:
         even = 0
-        number += 1
-        continue
+
+        if number == 10:
+            break
+        else: continue
+       
 print(even)
